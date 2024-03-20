66af5b8271bfee7b392d6c98a3663fb77b8e8cb2
Dragos Andrei <ronard@alpine.my.domain>
2024-03-20 15:05:11 +0100

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,37 @@
+## Set values
+# Hide welcome message
+pokemon-colorscripts --no-title -r
+set fish_greeting
+set VIRTUAL_ENV_DISABLE_PROMPT "1"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+mkdir -p 0700 /tmp/1000
+export XDG_RUNTIME_DIR=/tmp/1000
+
+
+## Export variable need for qt-theme
+if type "qtile" >> /dev/null 2>&1
+   set -x QT_QPA_PLATFORMTHEME "qt5ct"
+end
+
+# Set settings for https://github.com/franciscolourenco/done
+set -U __done_min_cmd_duration 10000
+set -U __done_notification_urgency_level low
+
+
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
+end
+
+    source ("/usr/bin/starship" init fish --print-full-init | psub)
+
+
+# Replace ls with exa
+alias ls='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias bsdfetch='clear && neofetch'
+alias python='python3.9'
+alias startx="ck-launch-session dbus-run-session Hyprland"
+alias sudo='doas'
